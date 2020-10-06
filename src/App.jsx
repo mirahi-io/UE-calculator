29cf6c87a9a472470b6ab1a6bca5d7c49f035797
Tim Simon <tim@mirahi.io>
2020-10-06 05:59:09 +0200

    Adding Kanye quote generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import CoursesList from "./components/CourseList/CoursesList";
 import Results from "./components/Results/Results";
 import AddCourseForm from "./components/AddCourseForm/AddCourseForm";
+import axios from "axios";
 
 const App = () => {
   const [courses, setCourses] = useState([]);
   const addCourse = (values) => setCourses((courses) => [...courses, values]);
+
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://api.kanye.rest/?format=json")
+      .then((response) => setQuote(response.data.quote));
+  }, []);
+
   return (
     <div className="container">
       <Results courses={courses} />
       <CoursesList courses={courses} />
       <AddCourseForm addCourse={addCourse} courses={courses} />
+      <p>{quote}</p>
     </div>
   );
 };
